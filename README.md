2527440745871c95c100e8d8b0f82862e4b81830
Jorge Danel <199034036+daneljorge@users.noreply.github.com>
2025-02-24 20:26:05 -0600

    app commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # sprint_7
 sprint 7 project
+
+this project show the functionality of two different plot options, scatter and histogram
+
+checking a box will show the selected plot option
---
+++ project_app.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+car_data = pd.read_csv(r"D:\JADP\Data Science\Repositories\sprint_7_project\us_vehicles.csv")
+
+st.header('Car data in the US')
+
+hist_checkbox = st.checkbox('Build histogram')
+disp_checkbox = st.checkbox('Build distribution')
+
+button = st.button('Build plot') # crear un botón
+
+if hist_checkbox and button: # when the button is clicked
+    # write a message
+    st.write('Creation of a histogram of the car sales dataset')
+    
+    # create histogram
+    
+    fig = px.histogram(car_data, x="odometer")
+    
+    # show interactive Plotly graph
+    
+    st.plotly_chart(fig, use_container_width=True) 
+
+if disp_checkbox and button: # when the button is clicked
+    # write a message
+    st.write('Creation of a scatter graph of the car sales dataset')
+    
+    # create scatter
+    
+    fig = px.scatter(car_data, x="odometer")
+    
+    # show interactive Plotly graph
+    
+    st.plotly_chart(fig, use_container_width=True) 
+
